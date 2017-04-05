c44584cbacb87bf1fa6a304d714259fbffec5c6e
makssie <maxneutro@gmail.com>
2017-04-05 16:43:06 -0300

    Adicionando Ng If para mostrar detalhes dos herois

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,21 +22,23 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+         <label>name: </label>
+         <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
     </div>
     <h3>My Heroes</h3>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes"  (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
     `,
-    //CSS da pagina//
-   styles: [`
+  //CSS da pagina//
+  styles: [`
   .selected {
     background-color: #CFD8DC !important;
     color: white;
@@ -86,13 +88,18 @@ const HEROES: Hero[] = [
   }
 `]
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-
+  selectedHero: Hero;
   heroes = HEROES;
-  
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
