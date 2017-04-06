812ddf8192db5b4e4e0610024c113f5e2dfa40b4
DanielRhazen <dma.morais29@gmail.com>
2017-04-06 10:55:19 -0300

    Organizando os dados dos herois em mock-hero.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 
-const HEROES: Hero[] = [
-  { id: 999, name: 'Eduardo Costa' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
 @Component({
   selector: 'my-app',
   templateUrl: "./views/app-component.html",
