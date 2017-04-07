7cc0191f4e3e10373b570d79a4f18c4bee956a0c
EduCosta1 <eduardoscosta@hotmail.com>
2017-04-06 23:19:21 -0300

    Alteraçao para mostrar os 4 melhores no dashboard

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,10 +1,22 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+
+import { HeroService } from './services/hero.service';
+import { Hero } from './hero';
 
 @Component({
     selector: 'my-dashboard',
     templateUrl: './views/dashboard-component.html'
 })
 
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+    heroes: Hero[] = []; //definindo uma array de Hero vazia
+
+    constructor(private heroService: HeroService){ }
+
+    ngOnInit(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1,5)); //promisse 
+    }
+
+
 
 }
